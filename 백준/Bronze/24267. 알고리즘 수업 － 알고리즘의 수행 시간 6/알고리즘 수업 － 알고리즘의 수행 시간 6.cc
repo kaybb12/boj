9f2691cc4ad9b72ae3adafc0fe59a364184b59b7
Jeong Jae Ho <117065754+kaybb12@users.noreply.github.com>
2024-01-24 17:30:20 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+	ll n;
+	cin >> n;
+	cout << n * (n - 1) * (n - 2) / (3 + 2 + 1) << "\n" << 3 << "\n";
+
+	return 0;
+}

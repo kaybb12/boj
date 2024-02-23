4bf5598a3b2dcca31b395dab5f21454e63e963ef
Jeong Jae Ho <117065754+kaybb12@users.noreply.github.com>
2024-02-23 19:20:45 +0900

    [Silver V] Title: 숫자 카드, Time: 216 ms, Memory: 5892 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -33,37 +33,18 @@ void print_cards_have(vector<int>& in_cards, const vector<int>& out_cards) {
 	cout << "\n";
 }
 
-void comapre_card(vector<int>& in_cards, vector<int>& out_cards) {
+void search_binary(vector<int>& in_cards, const vector<int>& out_cards) {
 	sort(in_cards.begin(), in_cards.end());
-	vector<bool> had_card(out_cards.size());
 
-	for (int i = 0; i < out_cards.size(); ++i) {
-		int right = in_cards.size() - 1;
-		int left = 0;
-		while (left <= right) {
-			int mid = (right + left) / 2;
-
-			if (in_cards[mid] == out_cards[i]) {
-				had_card[i] == true;
-				break;
-			}
-
-			if (in_cards[mid] > out_cards[i])
-				right = mid - 1;
-			else
-				left = mid + 1;
-		}
+	for (const auto& card : out_cards) {
+		cout << binary_search(in_cards.begin(), in_cards.end(), card) << " ";
 	}
-
-	for (const auto is_had : had_card)
-		cout << is_had << " ";
 
 	cout << "\n";
 }
 
 int main() {
 	cin.tie(nullptr);
-	cout.tie(nullptr);
 	ios_base::sync_with_stdio(false);
 
 	int n;
@@ -75,11 +56,13 @@ int main() {
 	int m;
 	cin >> m;
 	vector<int> out_cards(m);
-	for (int i = 0; i < m; ++i) 
+	for (int i = 0; i < m; ++i) {
 		cin >> out_cards[i];
+	}
 
-	print_cards_have(in_cards, out_cards);
+	//print_cards_have(in_cards, out_cards);
 	//comapre_card(in_cards, out_cards);
+	search_binary(in_cards, out_cards);
 
 	return 0;
 }
